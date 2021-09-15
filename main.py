dccca2a166ece04396f02f1f858bf2f4a780dc8e
mweltin <mweltinpdx@gmail.com>
2021-09-15 07:15:43 -0700

    fix has_winner logic check

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import environment.env as env
 import rules.rules as rules
 import random
 from copy import deepcopy
+
 random.seed(19)
+
 
 def print_hands_to_console(state):
     # print('Your left hand: ' + str(state[1][0]) + ' Your right hand: ' + str(state[1][1]))
     # print('Computer\'s left hand: ' + str(state[0][0]) + ' Computer\'s right hand: ' + str(state[0][1]))
-    print('your hand ' + str(state[1])  )
-    print('computer hand ' + str(state[0]) )
+    print('your hand ' + str(state[1]))
+    print('computer hand ' + str(state[0]))
 
 
 def play_sticks():
@@ -24,20 +26,20 @@ def play_sticks():
     else:
         active_player_idx = 0
 
-    has_winner = False
+    has_winner = None
     current_state = deepcopy(env.state_table[state_idx])
-    while not has_winner:
+    while has_winner is None:
         print_hands_to_console(current_state)
         if active_player_idx == 1:
             action = input(
                 f'Pick a Move:  0 = swap, 1 = left to left, 2 = left to right \n' + \
                 '3 = right to right 4 = right to left \n ')
-            current_state = rules.take_turn(current_state, active_player_idx, env.action_table[int(action)] )
+            current_state = rules.take_turn(current_state, active_player_idx, env.action_table[int(action)])
             active_player_idx = 0
         else:
             action = np.nanargmax(q_table[state_idx])
-            print('computer action:' + str(env.action_table[action]) )
-            current_state = rules.take_turn(current_state, active_player_idx, env.action_table[action] )
+            print('computer action:' + str(env.action_table[action]))
+            current_state = rules.take_turn(current_state, active_player_idx, env.action_table[action])
             active_player_idx = 1
 
         state_idx = env.state_table.index(current_state)
