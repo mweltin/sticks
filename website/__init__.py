1c8c5c28e745b471e2cc9b97e8743edfb1450bc7
mweltin <mweltinpdx@gmail.com>
2021-09-17 21:47:11 -0700

    add game django app and static assets

---
+++ website/__init__.py
@@ -0,0 +1,16 @@
+"""
+My short cool script
+Author: Cool Dude
+"""
+from cool import awesome
+
+
+def main():
+    """
+    This is the main function that does cool things.
+    """
+    awesome.run()
+
+
+if __name__ == '__main__':
+    main()
