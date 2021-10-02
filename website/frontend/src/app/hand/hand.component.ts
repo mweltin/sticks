9a0c9390b44f152bd8b03b99a508cee811f4fc99
mweltin <mweltinpdx@gmail.com>
2021-10-01 19:40:56 -0700

    add orientation property to app-hand dsl

--- website/frontend/src/app/hand/hand.component.ts
+++ website/frontend/src/app/hand/hand.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { throwError } from "rxjs";
 @Component({
   selector: 'app-hand',
   templateUrl: './hand.component.html',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HandComponent implements OnInit {
 
-  constructor() { }
+  imgSrc: String = "/angular/app/assets/one-a-l.png";
+
+  @Input()
+  orientation: string = '';
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+
+      switch(this.orientation)
+      {
+        case "left":
+          this.imgSrc = "/static/angular/app/assets/one-a-l.png";
+        break;
+
+        case "right":
+          this.imgSrc = "/static/angular/app/assets/one-a-r.png";
+        break;
+
+        default:
+          throwError("Hand orientation must be left or right.");
+      }
+
+      
+  }
+
+  handClick(){
+    alert("has been clicked");
   }
 
 }
