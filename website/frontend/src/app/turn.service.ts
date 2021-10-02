d2e1b3bf185445e1045d6af48c7aef1c855ef56a
mweltin <mweltinpdx@gmail.com>
2021-10-02 16:26:26 -0700

    add django turn endpoint and buid out ng turn service more

--- website/frontend/src/app/turn.service.ts
+++ website/frontend/src/app/turn.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TurnService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private turnEndpoint = '/';
+
+  takeATurn(state: object){
+    return this.http.post(this.turnEndpoint, {state});
+  }
 }
+
+
+
--- website/game/views.py
+++ website/game/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -11,3 +11,11 @@ def index(request):
         'welcome': 'Welcome to the sticks game:',
     }
     return HttpResponse(template.render(context, request))
+
+def turn(request):
+    data = {
+        'human': [1,1],
+        'qlearning': [2,3],
+        'takenBy': 'human'
+    }
+    return JsonResponse(data, safe=False)
