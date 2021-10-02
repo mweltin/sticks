9632d441e3f6994fc88dee9d3523ba9446c4607e
mweltin <mweltinpdx@gmail.com>
2021-10-02 15:20:17 -0700

    bind player finger counts to hand component

--- website/frontend/src/app/hand/hand.component.ts
+++ website/frontend/src/app/hand/hand.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, Input, Output, EventEmitter   } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange, SimpleChanges   } from '@angular/core';
 import { throwError } from "rxjs";
 @Component({
   selector: 'app-hand',
   templateUrl: './hand.component.html',
   styleUrls: ['./hand.component.css']
 })
-export class HandComponent implements OnInit {
+export class HandComponent implements OnInit, OnChanges{
 
   imgSrc: String = "/angular/app/assets/one-a-l.png";
-  fingers: number = 1;
+  numberToString: string = 'one';
+  rightLeftAbrv: string = 'r';
+  imageBase: string = "/static/angular/app/assets/";
 
   @Input()
   orientation: string = '';
+  @Input()
+  fingers: number = 1;
 
   @Output() 
   handClicked: EventEmitter<string> = new EventEmitter();
@@ -20,22 +24,54 @@ export class HandComponent implements OnInit {
     
   }
 
+  ngOnChanges(changes: SimpleChanges): void
+  {
+    for( let property in changes)
+    {
+      if(property === 'fingers')
+      {
+        switch(changes[property].currentValue)
+        {
+          case 0:
+            this.numberToString = "zero";
+            break;
+          case 1: 
+            this.numberToString = "one";
+            break;
+          case 2:
+            this.numberToString = "two";
+            break;
+          case 3: 
+            this.numberToString = "three";
+            break;
+          case 4:
+            this.numberToString = "four";
+          break;
+          default:
+            throwError("Bad finger value must be between 0 and 4");
+        }
+        this.imgSrc = this.imageBase+this.numberToString+"-a-"+this.rightLeftAbrv+".png";
+      }
+    }
+  }
+
   ngOnInit(): void {
 
       switch(this.orientation)
       {
         case "left":
-          this.imgSrc = "/static/angular/app/assets/one-a-l.png";
+          this.imgSrc = this.imageBase+"one-a-l.png";
+          this.rightLeftAbrv = 'l';
         break;
 
         case "right":
-          this.imgSrc = "/static/angular/app/assets/one-a-r.png";
+          this.imgSrc = this.imageBase+"one-a-r.png";
+          this.rightLeftAbrv = 'r';
         break;
 
         default:
           throwError("Hand orientation must be left or right.");
       }
-
       
   }
 
--- website/frontend/src/app/player/player.component.ts
+++ website/frontend/src/app/player/player.component.ts
@@ -10,15 +10,29 @@ export class PlayerComponent implements OnInit {
   @Input()
   playerType: string = '';
 
+  LFingers: number = 1;
+  RFingers: number = 1;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
   swap() {
     alert("perform swap action for " + this.playerType +"." );
   }
 
   handClickedHandler(hand: string) {
     console.log(this.playerType+"'s "+ hand + " has been clicked");
+    if(hand == "right")
+    {
+      this.RFingers = ++this.RFingers % 5;
+    }
+
+    if(hand == "left")
+    {
+      this.LFingers = ++this.LFingers % 5;
+    }
+    
   }
 }
