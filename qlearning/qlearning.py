0c56f7fcad2d9221e5ecbe7c6cd9c31659528de2
mweltin <mweltin@pattan.net>
2022-05-22 11:19:47 -0700

    use arg  parser to pass command line args to q-learning algorithm

--- qlearning/qlearning.py
+++ qlearning/qlearning.py
@@ -5,9 +5,19 @@ Author: Markus Weltin
 import numpy as np
 import random
 import environment.env as env
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
 
 
-def main():
+def main(
+        num_episodes=60000,
+        max_steps_per_episode=50,
+        learning_rate=0.1,
+        discount_rate=0.99,
+        min_exploration_rate=0.01,
+        exploration_decay_rate=0.001,
+):
     action_space_size = len(env.action_table)
     state_space_size = len(env.state_table)
 
@@ -16,33 +26,33 @@ def main():
     env.eliminate_invalid_actions(env.state_table, q_table)
 
     '''how many games to play'''
-    num_episodes = 60000
+    _num_episodes = num_episodes
     ''' how many turns to take in a game before starting a new game. '''
-    max_steps_per_episode = 50
+    _max_steps_per_episode = max_steps_per_episode
 
     '''learning rate how quickly the agent abandons the previous q value in the 
         qtable for a new q value for a give (state, action) pair.  
         The higher the learning rate the quicker the agent will adopt the new
         q value. '''
-    learning_rate = 0.1
-    discount_rate = 0.99
+    _learning_rate = learning_rate
+    _discount_rate = discount_rate
 
     '''The following values control the exploration / exploitation of the agent'''
     exploration_rate = 1
     max_exploration_rate = 1
-    min_exploration_rate = 0.01
-    exploration_decay_rate = 0.001
+    _min_exploration_rate = min_exploration_rate
+    _exploration_decay_rate = exploration_decay_rate
 
     rewards_all_episodes = []
 
     # Q-learning algorithm
-    for episode in range(num_episodes):
+    for episode in range(_num_episodes):
         # initialize new episode params
         state_idx = env.reset()  # state is the index in the env.state_table
         done = False
         rewards_current_episode = 0
 
-        for step in range(max_steps_per_episode):
+        for step in range(_max_steps_per_episode):
             # Exploration-exploitation trade-off
             # Take new action
             # Update Q-table
@@ -61,8 +71,9 @@ def main():
                 rewards_current_episode += reward
                 break
 
-            q_table[state_idx][action] = q_table[state_idx][action] * (1 - learning_rate) + \
-                learning_rate * (reward + discount_rate * np.nanargmax(q_table[new_state_idx]))
+            q_table[state_idx][action] = q_table[state_idx][action] * (1 - _learning_rate) + \
+                                         _learning_rate * (
+                                                 reward + _discount_rate * np.nanargmax(q_table[new_state_idx]))
 
             state_idx = new_state_idx
             rewards_current_episode += reward
@@ -75,13 +86,13 @@ def main():
             if done:
                 break
         # Exploration rate decay
-        exploration_rate = min_exploration_rate + \
-            (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * episode)
+        exploration_rate = _min_exploration_rate + \
+                           (max_exploration_rate - _min_exploration_rate) * np.exp(-_exploration_decay_rate * episode)
         # Add current episode reward to total rewards list
         rewards_all_episodes.append(rewards_current_episode)
 
     # Calculate and print the average reward per thousand episodes
-    rewards_per_thousand_episodes = np.split(np.array(rewards_all_episodes), num_episodes / 1000)
+    rewards_per_thousand_episodes = np.split(np.array(rewards_all_episodes), _num_episodes / 1000)
     count = 1000
 
     print("********Average reward per thousand episodes********\n")
@@ -106,4 +117,24 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Q-algorithm arguments')
+    parser.add_argument('--num_episodes', type=int, default=60000,
+                        help='how many games to play')
+    parser.add_argument('--max_steps_per_episode', type=int, default=50,
+                        help='Number of turns to take in a game before starting a new game.')
+    parser.add_argument('--learning_rate', type=float, default=0.1,
+                        help='rate at which algorithm updates new q-values')
+    parser.add_argument('--discount_rate', type=float, default=0.99,
+                        help='used to weight the old and new q-values when updating q-table')
+    parser.add_argument('--min_exploration_rate', type=float, default=0.01,
+                        help='minimum value for exploration/exploitation ration can get')
+    parser.add_argument('--exploration_decay_rate', type=float, default=0.001,
+                        help='speed at which exploration rate reaches minimum exploration rate')
+    args = parser.parse_args()
+
+    main(num_episodes=args.num_episodes,
+         max_steps_per_episode=args.max_steps_per_episode,
+         learning_rate=args.learning_rate,
+         discount_rate=args.discount_rate,
+         min_exploration_rate=args.min_exploration_rate,
+         exploration_decay_rate=args.exploration_decay_rate)
