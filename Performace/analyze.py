0932581311bdeda6ac94c2422d9583ebac89f07e
mweltin <mweltin@pattan.net>
2022-08-03 15:25:41 -0700

    convert data directory to variable

--- Performace/analyze.py
+++ Performace/analyze.py
@@ -5,24 +5,25 @@ import numpy as np
 def main():
     # save results  naming convention player 1 vs player 2,  player 1 always went first
     # qlearning vs dummy
-    datafiles = ["../data/qlearning/battle_data/gl_vs_dummy_results.csv",
-                 "../data/qlearning/battle_data/dummy_vs_ql_results.csv",
-                 "../data/qlearning/battle_data/ql_vs_wolf_results.csv",
-                 "../data/qlearning/battle_data/wolf_vs_ql_results.csv",
-                 "../data/qlearning/battle_data/ql_vs_dual_1_results.csv",
-                 "../data/qlearning/battle_data/dual_1_vs_ql_results.csv",
-                 "../data/qlearning/battle_data/ql_vs_dual_2_results.csv",
-                 "../data/qlearning/battle_data/dual_2_vs_ql_results.csv",
-                 "../data/wolf/battle_data/wolf_vs_dummy_results.csv",
-                 "../data/wolf/battle_data/dummy_vs_wolf_results.csv",
-                 "../data/wolf/battle_data/wolf_vs_dual_1_results.csv",
-                 "../data/wolf/battle_data/dual_1_vs_wolf_results.csv",
-                 "../data/wolf/battle_data/wolf_vs_dual_2_results.csv",
-                 "../data/wolf/battle_data/dual_2_vs_wolf_results.csv",
-                 "../data/dual/agent_1/battle_data/dual_1_vs_dummy_results.csv",
-                 "../data/dual/agent_1/battle_data/dummy_vs_dual_1_results.csv",
-                 "../data/dual/agent_2/battle_data/dual_2_vs_dummy_results.csv",
-                 "../data/dual/agent_2/battle_data/dummy_vs_dual_2_results.csv",
+    root_dir = '/Users/mweltin/data_report_results'
+    datafiles = [root_dir + "/qlearning/battle_data/gl_vs_dummy_results.csv",
+                 root_dir + "/qlearning/battle_data/dummy_vs_ql_results.csv",
+                 root_dir + "/qlearning/battle_data/ql_vs_wolf_results.csv",
+                 root_dir + "/qlearning/battle_data/wolf_vs_ql_results.csv",
+                 root_dir + "/qlearning/battle_data/ql_vs_dual_1_results.csv",
+                 root_dir + "/qlearning/battle_data/dual_1_vs_ql_results.csv",
+                 root_dir + "/qlearning/battle_data/ql_vs_dual_2_results.csv",
+                 root_dir + "/qlearning/battle_data/dual_2_vs_ql_results.csv",
+                 root_dir + "/wolf/battle_data/wolf_vs_dummy_results.csv",
+                 root_dir + "/wolf/battle_data/dummy_vs_wolf_results.csv",
+                 root_dir + "/wolf/battle_data/wolf_vs_dual_1_results.csv",
+                 root_dir + "/wolf/battle_data/dual_1_vs_wolf_results.csv",
+                 root_dir + "/wolf/battle_data/wolf_vs_dual_2_results.csv",
+                 root_dir + "/wolf/battle_data/dual_2_vs_wolf_results.csv",
+                 root_dir + "/dual/agent_1/battle_data/dual_1_vs_dummy_results.csv",
+                 root_dir + "/dual/agent_1/battle_data/dummy_vs_dual_1_results.csv",
+                 root_dir + "/dual/agent_2/battle_data/dual_2_vs_dummy_results.csv",
+                 root_dir + "/dual/agent_2/battle_data/dummy_vs_dual_2_results.csv",
                  ]
     results = [['player 1', 'player 2', 'wins', 'losses', 'draws', 'performance']]
 
@@ -50,6 +51,8 @@ def main():
                results,
                delimiter=", ",
                fmt='% s')
+
+    exit()
 
 
 if __name__ == '__main__':
